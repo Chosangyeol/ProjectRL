02b2f6d394ea2b8d247083dacf3a3beb2052e2c0
Chosangyeol <tankg159@gmail.com>
2025-09-25 13:11:24 +0900

    1
    1

---
+++ Assets/02. Scrpits/Enemy/SO/EnemySO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New EnemySO", menuName = "EnemySO")]
+public class EnemySO : ScriptableObject
+{
+    
+}
