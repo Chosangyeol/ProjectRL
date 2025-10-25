678612c294556e0a8afb24631739c2d086951f3f
HW-Howdy <84364883+HW-Howdy@users.noreply.github.com>
2025-10-25 21:34:35 +0900

    [ADD] 업적 시스템 기초
    이벤트 버스를 활용하여 초안 작성
    이제 테스트랑 구현만 하면 됨
    언제 함 ㅠ.ㅠ

---
+++ Assets/02. Scrpits/Achievement/AchievementSystem.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using UnitySubCore.Singleton;
+
+namespace Achievement
+{
+	public class AchievementSystem : ASingleton<AchievementSystem>
+	{
+		private List<IAchievement> achievements;
+		private EventBus eventbus;
+
+		public AchievementSystem()
+		{
+			achievements = new List<IAchievement>();
+			eventbus = new EventBus();
+			Initailize();
+			return;
+		}
+
+		public void Initailize()
+		{
+			achievements.Clear();
+			// TODO! - setup
+			foreach (IAchievement achievement in achievements)
+			{
+				achievement.Initialize(eventbus);
+			}
+			return ;
+		}
+
+		public void Shutdown()
+		{
+			foreach (IAchievement achievement in achievements)
+			{
+				achievement.Shutdown(eventbus);
+			}
+			achievements.Clear();
+			return ;
+		}
+
+		public void Subscribe<GType01>(Action<GType01> handler) where GType01 : IEvent
+		{
+			eventbus.Subscribe(handler);
+			return;
+		}
+
+		public void Unsubscribe<GType01>(Action<GType01> handler) where GType01 : IEvent
+		{
+			eventbus.Unsubscribe(handler);
+			return;
+		}
+
+		public void Publish<GType01>(GType01 evt) where GType01 : IEvent
+		{
+			eventbus.Publish(evt);
+			return;
+		}
+	}
+}
---
+++ Assets/02. Scrpits/Achievement/Achievements/AchievementTest.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace Achievement
+{
+	public class AchievementTest : IAchievement
+	{
+		public string Name { get; private set; }
+		public bool IsUnlocked { get; private set; }
+
+		public AchievementTest()
+		{
+			Name = "Test_Achievement";
+			IsUnlocked = false;
+		}
+
+		public void Initialize(EventBus eventBus)
+		{
+			eventBus.Subscribe<SEventTest>(TestFunction);
+			return ;
+		}
+		
+		public void Shutdown(EventBus eventBus)
+		{
+			eventBus.Unsubscribe<SEventTest>(TestFunction);
+			return ;
+		}
+
+		public void TestFunction(SEventTest evt)
+		{
+			if (IsUnlocked)
+				return ;
+			IsUnlocked = true;
+			Debug.Log("Test Achievement Unlock!");
+			return ;
+		}
+	}
+}
