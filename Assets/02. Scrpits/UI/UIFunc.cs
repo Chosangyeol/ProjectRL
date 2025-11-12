e8f8c9858cb4509f5f68341bc390acbd25b1b154
CoberdPL <104338469+CoberdPL@users.noreply.github.com>
2025-11-12 19:00:59 +0900

    Add files via upload

--- Assets/02. Scrpits/UI/UIFunc.cs
+++ Assets/02. Scrpits/UI/UIFunc.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Config;
 
 namespace UI
 {
     public class UIFunc : MonoBehaviour
     {
-        
+        ConfigUserInput CUI;
+        public GameObject PausePanel;
+        public GameObject BlurPanel;
+
+        private void Update()
+        {
+            if (CUI.GetKeyDown("keyPause")) // Esc
+            {
+                // 나중에 특정 씬에서는 안 펼쳐지게 하면 될 듯
+                ObjectShow(PausePanel);
+                ObjectShow(BlurPanel);
+            }
+        }
+
         public void ChangeScene(string sceneName)
         {
             UI.SceneManage.LoadSceneManagement.LoadScene(sceneName);
@@ -26,7 +40,5 @@ namespace UI
         {
             obj.SetActive(false);
         }
-
-        
     }
 }
