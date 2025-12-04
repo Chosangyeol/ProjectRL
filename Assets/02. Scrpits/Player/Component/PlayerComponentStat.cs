2520c3b6abd0235b4fa1279f481ced2438fa3f6d
HW-Howdy <84364883+HW-Howdy@users.noreply.github.com>
2025-12-04 10:34:49 +0900

    [ADD] PlayerComponentStat 내부 stat 이원화
    합연산과 곱연산을 위해 origin과 edited를 구분함

--- Assets/02. Scrpits/Player/Component/PlayerComponentStat.cs
+++ Assets/02. Scrpits/Player/Component/PlayerComponentStat.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Cryptography;
 
 namespace Player.Component
 {
@@ -7,10 +8,13 @@ namespace Player.Component
 		public PlayerModel playerModel;
 
 		private SPlayerStat origin;
+		private SPlayerStat edited;
 
-		public SPlayerStat Stat { get => origin; }
+		public SPlayerStat Stat { get => edited; }
 
 		public delegate void StatCalculator(ref SPlayerStat stat);
+
+		private event StatCalculator	ActionCalculateStat;
 
 		public PlayerComponentStat(PlayerModel model, PlayerComponentStatSO so)
 		{
@@ -22,12 +26,35 @@ namespace Player.Component
 		public void EditOriginStat(StatCalculator calculator)
 		{
 			calculator(ref origin);
+			RecalculateStat();
+			return ;
+		}
+
+		public void AddCalculateStat(StatCalculator calculator)
+		{
+			ActionCalculateStat += calculator;
+			RecalculateStat();
+			return ;
+		}
+
+		public void RemoveCalculateStat(StatCalculator calculator)
+		{
+			ActionCalculateStat -= calculator;
+			RecalculateStat();
 			return ;
 		}
 
 		public void AddStat(SPlayerStat add)
 		{
 			origin += add;
+			RecalculateStat();
+			return ;
+		}
+
+		private void RecalculateStat()
+		{
+			edited = origin;
+			ActionCalculateStat(ref edited);
 			return ;
 		}
 
@@ -35,23 +62,23 @@ namespace Player.Component
 		{
 			if (add < 0)
 				return (0);
-			origin.shield += add;
-			if (origin.shield > Int32.MaxValue)
+			edited.shield += add;
+			if (edited.shield > Int32.MaxValue)
 			{
-				add = origin.shield - Int32.MaxValue;
+				add = edited.shield - Int32.MaxValue;
 			}
 			return (add);
 		}
 
 		public int RemoveShield(int remove)
 		{
-			if (origin.shield <= 0 || remove <= 0)
+			if (edited.shield <= 0 || remove <= 0)
 				return (remove);
-			origin.shield -= remove;
-			if (origin.shield < 0)
+			edited.shield -= remove;
+			if (edited.shield < 0)
 			{
-				remove += origin.shield;
-				origin.shield = 0;
+				remove += edited.shield;
+				edited.shield = 0;
 			}
 			return (remove);
 		}
@@ -60,11 +87,11 @@ namespace Player.Component
 		{
 			if (heal < 0)
 				return (0);
-			origin.hpCurrent += heal;
-			if (origin.hpCurrent > origin.hpMax)
+			edited.hpCurrent += heal;
+			if (edited.hpCurrent > edited.hpMax)
 			{
-				heal = origin.hpCurrent - origin.hpMax;
-				origin.hpCurrent = origin.hpMax;
+				heal = edited.hpCurrent - edited.hpMax;
+				edited.hpCurrent = edited.hpMax;
 			}
 			return (heal);
 		}
@@ -73,23 +100,23 @@ namespace Player.Component
 		{
 			if (damage <= 0)
 				return (0);
-			if (origin.shield <= damage)
+			if (edited.shield <= damage)
 			{
-				damage -= origin.shield;
-				origin.shield = 0;
+				damage -= edited.shield;
+				edited.shield = 0;
 			}
 			else
 			{
-				origin.shield -= damage;
+				edited.shield -= damage;
 				damage = 0;
 			}
-			origin.hpCurrent = Math.Max(origin.hpCurrent - damage, 0);
+			edited.hpCurrent = Math.Max(edited.hpCurrent - damage, 0);
 			return (damage);
 		}
 
 		public bool IsAlive()
 		{
-			return (origin.hpCurrent > 0);
+			return (edited.hpCurrent > 0);
 		}
 
 		public int AddExp(int exp)
@@ -121,36 +148,37 @@ namespace Player.Component
 
 		public void CountJump()
 		{
-			origin.jumpCountCurrent++;
+			edited.jumpCountCurrent++;
 			return ;
 		}
 
 		public void ResetJumpCount()
 		{
-			origin.jumpCountCurrent = 0;
+			edited.jumpCountCurrent = 0;
 			return ;
 		}
 
 		public bool CanJump()
 		{
-			return (origin.jumpCountCurrent < origin.jumpCountMax);
+			return (edited.jumpCountCurrent < edited.jumpCountMax);
 		}
 
 		public float GetSpeed(bool isSprint)
 		{
 			if (isSprint)
-				return (origin.speedSprint);
-			return (origin.speedMove);
+				return (edited.speedSprint);
+			return (edited.speedMove);
 		}
 
 		public float GetJumpPower()
 		{
-			return (origin.jumpPower);
+			return (edited.jumpPower);
 		}
 
 		private void Equalize(PlayerComponentStatSO so)
 		{
 			origin.Equalize(so);
+			edited = origin;
 			return ;
 		}
 	}
